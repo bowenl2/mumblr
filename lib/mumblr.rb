29401577d84bd3ba0ad60c385aace9a92685f85c
Liam Bowen <LiamBowen@gmail.com>
2014-03-12 21:03:28 -0400

    Download, progress bar, pass the correct parameters from the command line in.

--- lib/mumblr.rb
+++ lib/mumblr.rb
@@ -1,13 +1,51 @@
 require "mumblr/version"
 require "tumblr_client"
 require "pry"
+require "open-uri"
+require "progressbar"
 
 class Mumblr
   def initialize(base_hostname)
     @base_hostname = base_hostname
+    @dest_base = File.expand_path('~/mumblr-data')
+    populate_filecache
     normalize_base_hostname
   end
 
+  # TODO
+  def populate_filecache
+    # Crawl everything in @dest_base
+  end
+
+  def download(file_url, username, origin=nil)
+    dest_path = File.join(@dest_base, username, origin)
+    FileUtils.mkdir_p(dest_path) unless File.exists?(dest_path)
+    dest_path = File.join(dest_path, File.basename(file_url))
+    # TODO Check for cache
+    if File.exists?(dest_path)
+      STDERR.puts("Skipping #{dest_path} (exists)")
+      return
+    end
+    pbar = nil
+    begin
+      open(dest_path, 'wb') do |dest_file|
+        open(file_url,
+             content_length_proc: lambda {|t|
+               if t && 0 < t
+                 pbar = ProgressBar.new("...", t)
+                 pbar.file_transfer_mode
+               end
+             },
+             progress_proc: lambda {|s|
+               pbar.set s if pbar
+             }) do |f|
+          IO.copy_stream(f, dest_file)
+        end
+      end
+    rescue
+      STDERR.puts("error with #{file_url}")
+    end
+  end
   # options:
   #
   # types: array of: text, quote, link, answer, video, audio, photo, chat
@@ -18,25 +56,31 @@ class Mumblr
     unless @raw_posts
       @raw_posts = client.posts(@base_hostname, options)
     end
-
     extract_from_posts(@raw_posts['posts'])
   end
 
   def blog_likes_content(options={})
+    tumblr_params = {
+      offset: 0,
+      limit: 20
+    }
     unless @raw_likes
-      count_needed = options[:count]
       @raw_likes = []
       loop do
-        options[:offset] = @raw_likes.length
-        likes_res = client.blog_likes(@base_hostname, options)
+        STDERR.puts "So far wehave #{@raw_likes.length}"
+        tumblr_params[:offset] = @raw_likes.length
+        likes_res = client.blog_likes(@base_hostname, tumblr_params)
+        # The counts of total_likes are actually inaccurate.
+        break if likes_res['liked_posts'].length == 0
+        STDERR.puts "Just got #{likes_res['liked_posts'].length} more"
         @raw_likes += likes_res['liked_posts']
-        if @raw_likes.length >= likes_res['liked_count'].to_i
-          break
-        end
+        total_likes = likes_res['liked_count'].to_i
+        STDERR.puts "There are a total of: #{total_likes}"
       end
     end
 
-    extract_from_posts(@raw_likes)
+    url_list = extract_from_posts(@raw_likes)
+    url_list.each { |url| download(url, @base_hostname, 'likes') }
   end
 
   private
--- mumblr.gemspec
+++ mumblr.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "tumblr_client"
   spec.add_runtime_dependency "thor"
+  spec.add_runtime_dependency "progressbar"
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
